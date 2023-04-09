ef883c7821e6b4c5bf1bc43e69fe375b6c1afa99
Andreye <drontop1999@mail>
2023-04-09 23:11:15 +0300

    Commit Seminars

--- ThirdLection.py
+++ ThirdLection.py
@@ -1,1 +1,22 @@
-# Урок 3. Функции, рекурсия, алгоритмы
+# print("Введите имя: ")
+# a = input()
+# print("Hello", a, "!")
+
+#
+# a = [1,2,3,4,5]
+# x = lambda b: b % 2 == 0
+# print(list(filter(x,a)))
+
+a = [1,10,3,4,5,6]
+b = []
+for i, x in enumerate(a):
+    if (i+1) % 3 == 0:
+        b.append(a[i])
+print(b)
+
+
+
+
+
+
+
